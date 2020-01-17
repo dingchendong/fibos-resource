a15ba4c811023f4482f9a5046fc00dc70f1076c6
dingchendong <chendong.ding@d3j.io>
2020-01-17 10:10:53 +0800

    update:fibos-system

--- example/index.js
+++ example/index.js
@@ -1,6 +1,45 @@
-require("../lib/before.js");
-
+const fs = require("fs");
 const fibos = require("fibos");
+
+let check_UTC = () => {
+	let date = new Date();
+	if (ENV === 'prod' && date.getTimezoneOffset() !== 0) {
+		console.error("now the server's time zone is not UTC");
+		process.exit();
+	}
+}
+
+let setLogs = (logPath) => {
+	if (!fs.exists(logPath)) fs.mkdir(logPath);
+
+	console.add([{
+		type: "console",
+		levels: [console.FATAL, console.ALERT, console.CRIT, console.ERROR, console.WARN, console.NOTICE, console.INFO],
+	}, {
+		type: "file",
+		levels: [console.FATAL, console.ALERT, console.CRIT, console.ERROR],
+		path: logPath + "error.log",
+		split: "hour",
+		count: 128
+	}, {
+		type: "file",
+		levels: [console.WARN],
+		path: logPath + "warn.log",
+		split: "hour",
+		count: 128
+	}, {
+		type: "file",
+		levels: [console.INFO],
+		path: logPath + "access.log",
+		split: "hour",
+		count: 128
+	}]);
+}
+
+check_UTC();
+
+setLogs("../logs/");
+
 fibos.config_dir = "./data";
 fibos.data_dir = "./data";
 fibos.load("http", {
@@ -10,8 +49,13 @@ fibos.load("http", {
 	"verbose-http-errors": true
 });
 
+let params = {
+	DBconnString: "mysql://root:123456@127.0.0.1/absdb",
+	p2p_address: ["210.74.14.247:9801"]
+}
+
 fibos.load("net", {
-	"p2p-peer-address": CONFIG["p2p-peer-address"],
+	"p2p-peer-address": params.p2p_address,
 	"p2p-listen-endpoint": "0.0.0.0:9870"
 });
 
@@ -29,12 +73,7 @@ fibos.load("emitter");
 // [fibos - tracker]
 const Tracker = require("fibos-tracker");
 
-let params = {
-	httpEndpoint: "http://210.74.14.247:9090",
-	DBconnString: "mysql://root:123456@127.0.0.1/absdb"
-}
-
-Tracker.Config.DBconnString = CONFIG["DBconnString"];
+Tracker.Config.DBconnString = params.DBconnString;
 Tracker.Config.replay = true;
 const tracker = new Tracker();
 
